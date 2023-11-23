292bfe10e7f9a9bde68282eda1c99366fe945db5
AromaRich11 <33459821+AromaRich11@users.noreply.github.com>
2023-11-23 15:45:00 +0300

    реализован расчет метода риска шарпа по ручному вводу коина

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ from datetime import datetime
 def coin_listing():
     url = 'https://api.binance.com/api/v3/exchangeInfo' #по этой ссылке есть тонна инфы на коины
     call = requests.get(url, timeout=10)
-    cryptocurrency = call.json()['symbols'] #и по параметру symbols находим отрезок наименования коина
+    cryptocurrency = call.json()['baseAsset'] #и по параметру symbols находим отрезок наименования коина
+    print(cryptocurrency)
 
     symbol = [] #циклом прошлись по всем коинам по ключу baseAsset и заносим в список symbol
 
     for currency in cryptocurrency:
+        print(currency)
         symbol.append(currency.get('baseAsset'))
     return symbol
 
 
 x_list = coin_listing() #вызвали фукцию с соxранением в переменную
-
+print(x_list)
 
 def symbol_coins(x):
     coin = x_list[x] + 'USDT' #в этой переменной save коин + его цена
@@ -33,15 +35,15 @@ def symbol_coins(x):
     new_cryptocurrency = new_call.json()
     price = new_cryptocurrency.get('price') #вытянули цену коина
 
-    with open("coin_list.txt", "a", encoding='utf-8') as file: #запись в файл
-        file.write(f'{x_list[x]}:{price}' + '\n')
+    """with open("coin_list.txt", "a", encoding='utf-8') as file: #запись в файл
+        file.write(f'{x_list[x]}:{price}' + '\n')"""
 
     return x_list[x]
 
 
 if __name__ == '__main__':
     start_time = datetime.now()
-    with Pool(26) as p: #multiprocessing, спасибо тебе!
+    with Pool(26) as p: #multiprocessing, спасибо тебе! но тут стоит еще и почитать за multitreding
         print(p.map(symbol_coins, list(range(len(x_list)))))
         print(datetime.now() - start_time)
 
